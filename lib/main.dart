90ab4dcb4fb76ae4c2c5211497f60b5d3cdf220e
JaeHo Song <thruthesky@gmail.com>
2023-09-04 16:33:32 +0800

    updating the title

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("FireFlutter Site"),
+        title: const Text("FireFlutter Site 2"),
       ),
       body: Container(),
     );
